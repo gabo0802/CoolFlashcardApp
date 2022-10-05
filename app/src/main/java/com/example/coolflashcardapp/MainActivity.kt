8054967935545f38ae0135fece9aaf4c979a4dcf
gabo0802 <gabriel.castejon@ufl.edu>
2022-10-04 22:51:18 -0400

    Lab 3 initial Push

--- app/src/main/java/com/example/coolflashcardapp/MainActivity.kt
+++ app/src/main/java/com/example/coolflashcardapp/MainActivity.kt
@@ -12,12 +12,27 @@ import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var flashcardDatabase: FlashcardDatabase
+    var allFlashcards = mutableListOf<Flashcard>()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var currentCardDisplayedIndex = 0
+        flashcardDatabase = FlashcardDatabase(this)
+        flashcardDatabase.initFirstCard()
         val flashcardQuestion = findViewById<TextView>(R.id.flashcard_question)
         val flashcardAnswer = findViewById<TextView>(R.id.flashcard_answer)
+
+        allFlashcards = flashcardDatabase.getAllCards().toMutableList()
+        if (allFlashcards.size > 0) {
+            flashcardQuestion.text = allFlashcards[0].question
+            flashcardAnswer.text = allFlashcards[0].answer
+        }
+
         flashcardQuestion.setOnClickListener {
             flashcardQuestion.visibility = View.INVISIBLE
             flashcardAnswer.visibility = View.VISIBLE
@@ -40,11 +55,18 @@ class MainActivity : AppCompatActivity() {
                     val question = data.getStringExtra("question")
                     val answer = data.getStringExtra("answer")
 
+                    Log.i("MainActivity", "Question: $question")
+                    Log.i("MainActivity", "Answer: $answer")
+
                     flashcardQuestion.text = question
                     flashcardAnswer.text = answer
 
-                    Log.i("MainActivity", "Question: $question")
-                    Log.i("MainActivity", "Answer: $answer")
+                    if(question != null && answer != null) {
+                        flashcardDatabase.insertCard(Flashcard(question.toString(), answer.toString()))
+                        allFlashcards = flashcardDatabase.getAllCards().toMutableList()
+                    } else {
+                        Log.e("TAG","Missing question or answer to input into database. Question is $question and Answer is $answer")
+                    }
                 } else {
                     Log.i("MainActivity", "Returned null data from AddCardActivity")
                 }
@@ -55,5 +77,27 @@ class MainActivity : AppCompatActivity() {
             resultLauncher.launch(intent)
         }
 
+        findViewById<View>(R.id.next).setOnClickListener {
+            if(allFlashcards.size == 0) {
+                return@setOnClickListener
+            }
+
+            currentCardDisplayedIndex++
+
+            if(currentCardDisplayedIndex >= allFlashcards.size) {
+                Snackbar.make(
+                    findViewById<TextView>(R.id.flashcard_question),
+                    "You've reached the end of the cards, going back to start.",
+                    Snackbar.LENGTH_SHORT)
+                    .show()
+                currentCardDisplayedIndex = 0
+            }
+
+            allFlashcards = flashcardDatabase.getAllCards().toMutableList()
+            val (question, answer) = allFlashcards[currentCardDisplayedIndex]
+
+            findViewById<TextView>(R.id.flashcard_answer).text = answer
+            findViewById<TextView>(R.id.flashcard_question).text = question
+        }
     }
 }
